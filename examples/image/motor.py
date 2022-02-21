02e84163ade3da8e0bd3368421d12034d372936d
okosa <okosadumebi@gmail.com>
2022-02-21 11:22:54 -0700

    motor test code and new neural network model

---
+++ examples/image/motor.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+servoPIN = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+recycle = 3.5
+metal = 5.8
+compost = 8.16
+other = 10.5
+
+
+p = GPIO.PWM(servoPIN, 50) # GPIO 18 for PWM with 50Hz
+p.start(2.5) # Initialization
+try:
+  while True:
+    p.ChangeDutyCycle(recycle)
+    time.sleep(2)
+    p.ChangeDutyCycle(metal)
+    time.sleep(2)
+    p.ChangeDutyCycle(compost)
+    time.sleep(2)
+    p.ChangeDutyCycle(other)
+    time.sleep(2)
+except KeyboardInterrupt:
+  p.stop()
+  #GPIO.cleanup()
+  
+finally:
+    GPIO.cleanup()

6a7094d52de49da492981a6c89d270b2b4895d7f
okosa <okosadumebi@gmail.com>
2022-02-24 15:01:21 -0700

    DVD drive

--- examples/image/motor.py
+++ examples/image/motor.py
@@ -1,127 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Python Script
+# https://www.electronicshub.org/raspberry-pi-l298n-interface-tutorial-control-dc-motor-l298n-raspberry-pi/
+
 import RPi.GPIO as GPIO
+from time import sleep
+
+in1 = 17
+in2 = 18
+en = 23
+
 import time
-
 GPIO.setwarnings(False)
-servoPIN = 18
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(en, GPIO.OUT)
+p = GPIO.PWM(en, 50)
+Duty_cycle = 30
+p.start(2.5)
 
-recycle = 3.5
-metal = 5.8
-compost = 8.16
-other = 10.5
 
-
-p = GPIO.PWM(servoPIN, 50) # GPIO 18 for PWM with 50Hz
-p.start(2.5) # Initialization
-try:
-   while True:
-        p.ChangeDutyCycle(recycle)
-        time.sleep(1)
-        p.ChangeDutyCycle(recycle+0.2)
-        time.sleep(1)
-        p.ChangeDutyCycle(recycle+0.4)
-        time.sleep(1)
-        p.ChangeDutyCycle(recycle+0.6)
-        time.sleep(1)
-        p.ChangeDutyCycle(recycle+0.8)
-        time.sleep(1)
-        p.ChangeDutyCycle(recycle+1.0)
-        time.sleep(1)
-        p.ChangeDutyCycle(recycle+1.2)
-        time.sleep(1)
-        p.ChangeDutyCycle(recycle+1.4)
-        time.sleep(1)
-        p.ChangeDutyCycle(recycle+1.6)
-        time.sleep(1)
-        p.ChangeDutyCycle(recycle+1.8)
-        time.sleep(1)
-        p.ChangeDutyCycle(recycle+2.0)
-        time.sleep(1)
-        p.ChangeDutyCycle(recycle+2.2)
-        time.sleep(1)
+while 1:
+            GPIO.output(in1, GPIO.HIGH)
+            GPIO.output(in2, GPIO.LOW)
+            p.ChangeDutyCycle(Duty_cycle)
+            time.sleep(2)
+            
+            GPIO.output(in1, GPIO.LOW)
+            GPIO.output(in2, GPIO.HIGH)
+            p.ChangeDutyCycle(Duty_cycle )
+            time.sleep(2)
+            
+            GPIO.output(in1, GPIO.HIGH)
+            GPIO.output(in2, GPIO.LOW)
+            p.ChangeDutyCycle(Duty_cycle )
+            time.sleep(2)
+            
+            GPIO.output(in1, GPIO.LOW)
+            GPIO.output(in2, GPIO.HIGH)
+            p.ChangeDutyCycle(Duty_cycle )
+            time.sleep(2)
+            
+            
         
-        p.ChangeDutyCycle(metal)
-        time.sleep(1)
-        p.ChangeDutyCycle(metal+0.2)
-        time.sleep(1)
-        p.ChangeDutyCycle(metal+0.4)
-        time.sleep(1)
-        p.ChangeDutyCycle(metal+0.6)
-        time.sleep(1)
-        p.ChangeDutyCycle(metal+0.8)
-        time.sleep(1)
-        p.ChangeDutyCycle(metal+1.0)
-        time.sleep(1)
-        p.ChangeDutyCycle(metal+1.2)
-        time.sleep(1)
-        p.ChangeDutyCycle(metal+1.4)
-        time.sleep(1)
-        p.ChangeDutyCycle(metal+1.6)
-        time.sleep(1)
-        p.ChangeDutyCycle(metal+1.8)
-        time.sleep(1)
-        p.ChangeDutyCycle(metal+2.0)
-        time.sleep(1)
-        p.ChangeDutyCycle(metal+2.2)
-        time.sleep(1)
-        
-        p.ChangeDutyCycle(compost)
-        time.sleep(1)
-        p.ChangeDutyCycle(compost+0.2)
-        time.sleep(1)
-        p.ChangeDutyCycle(compost+0.4)
-        time.sleep(1)
-        p.ChangeDutyCycle(compost+0.6)
-        time.sleep(1)
-        p.ChangeDutyCycle(compost+0.8)
-        time.sleep(1)
-        p.ChangeDutyCycle(compost+1.0)
-        time.sleep(1)
-        p.ChangeDutyCycle(compost+1.2)
-        time.sleep(1)
-        p.ChangeDutyCycle(compost+1.4)
-        time.sleep(1)
-        p.ChangeDutyCycle(compost+1.6)
-        time.sleep(1)
-        p.ChangeDutyCycle(compost+1.8)
-        time.sleep(1)
-        p.ChangeDutyCycle(compost+2.0)
-        time.sleep(1)
-        p.ChangeDutyCycle(compost+2.2)
-        time.sleep(1)
-        
-        p.ChangeDutyCycle(other)
-        time.sleep(1)
-        p.ChangeDutyCycle(other+0.2)
-        time.sleep(1)
-        p.ChangeDutyCycle(other+0.4)
-        time.sleep(1)
-        p.ChangeDutyCycle(other+0.6)
-        time.sleep(1)
-        p.ChangeDutyCycle(other+0.8)
-        time.sleep(1)
-        p.ChangeDutyCycle(other+1.0)
-        time.sleep(1)
-        p.ChangeDutyCycle(other+1.2)
-        time.sleep(1)
-        p.ChangeDutyCycle(other+1.4)
-        time.sleep(1)
-        p.ChangeDutyCycle(other+1.6)
-        time.sleep(1)
-        p.ChangeDutyCycle(other+1.8)
-        time.sleep(1)
-        p.ChangeDutyCycle(other+2.0)
-        time.sleep(1)
-        p.ChangeDutyCycle(other+2.2)
-        time.sleep(1)
-    
    
-        
-    
-except KeyboardInterrupt:
-  p.stop()
-  #GPIO.cleanup()
-  
-finally:
-    GPIO.cleanup()

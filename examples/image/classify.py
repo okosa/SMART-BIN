9e2215b4843abfa40315801ece4d50f0478dab2a
okosa <okosadumebi@gmail.com>
2022-02-26 13:30:57 -0700

    IR sensor integrated with camera

--- examples/image/classify.py
+++ examples/image/classify.py
@@ -121,6 +121,7 @@ def main(argv):
             next_frame = 0 # limit to ~10 fps here
 
             for res, img in runner.classifier(videoCaptureDeviceId):
+               if GPIO.input(IR_out) == False:
                 if (next_frame > now()):
                     time.sleep((next_frame - now()) / 1000)
 

ec1cd020dc3c5246ef61502f6e3bd790d56536ec
okosa <okosadumebi@gmail.com>
2022-03-05 11:59:01 -0700

    image processing module isolated

--- examples/image/classify.py
+++ examples/image/classify.py
@@ -1,34 +1,42 @@
+#!/usr/bin/env python
+ 
 import device_patches       # Device specific patches for Jetson Nano (needs to be before importing cv2)
-
+ 
 import cv2
 import os
 import sys, getopt
 import signal
 import time
 import RPi.GPIO as GPIO
-from motor_test import *
-
-
+from motor_test import * 
+ 
 GPIO.setwarnings(False)
+
+recycle = 3.5
+metal = 5.8
+compost = 8.16
+other = 10.5
+ 
 
 IR_out = 24
 GPIO.setup(IR_out, GPIO.IN)
-
+ 
+p.start(2.5) # Initialization
 from edge_impulse_linux.image import ImageImpulseRunner
 from gpiozero import LED
-
+ 
 # led = LED(17)
-
-
+ 
+ 
 runner = None
 # if you don't want to see a camera preview, set this to False
 show_camera = True
 if (sys.platform == 'linux' and not os.environ.get('DISPLAY')):
     show_camera = False
-
+ 
 def now():
     return round(time.time() * 1000)
-
+ 
 def get_webcams():
     port_ids = []
     for port in range(5):
@@ -44,112 +52,125 @@ def get_webcams():
                 port_ids.append(port)
             camera.release()
     return port_ids
-
+ 
 def sigint_handler(sig, frame):
     print('Interrupted')
     if (runner):
         runner.stop()
     sys.exit(0)
-
+ 
 signal.signal(signal.SIGINT, sigint_handler)
-
+ 
 def help():
     print('python classify.py <path_to_model.eim> <Camera port ID, only required when more than 1 camera is present>')
+ 
+def imageProcessing(again, argv):
+    while again == 1:
 
-
-
-def main(argv):
-    try:
-        opts, args = getopt.getopt(argv, "h", ["--help"])
-    except getopt.GetoptError:
-        help()
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt in ('-h', '--help'):
-            help()
-            sys.exit()
-
-    if len(args) == 0:
-        help()
-        sys.exit(2)
-
-    model = args[0]
-
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    modelfile = os.path.join(dir_path, model)
-
-    print('MODEL: ' + modelfile)
-
-    with ImageImpulseRunner(modelfile) as runner:
         try:
-            model_info = runner.init()
-            print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
-            labels = model_info['model_parameters']['labels']
-            if len(args)>= 2:
-                videoCaptureDeviceId = int(args[1])
-            else:
-                port_ids = get_webcams()
-                if len(port_ids) == 0:
-                    raise Exception('Cannot find any webcams')
-                if len(args)<= 1 and len(port_ids)> 1:
-                    raise Exception("Multiple cameras found. Add the camera port ID as a second argument to use to this script")
-                videoCaptureDeviceId = int(port_ids[0])
+            opts, args = getopt.getopt(argv, "h", ["--help"])
+        except getopt.GetoptError:
+            help()
+            sys.exit(2)
+     
+        for opt, arg in opts:
+            if opt in ('-h', '--help'):
+                help()
+                sys.exit()
+     
+        if len(args) == 0:
+            help()
+            sys.exit(2)
+     
+        model = args[0]
+     
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        modelfile = os.path.join(dir_path, model)
+     
+        print('MODEL: ' + modelfile)
+     
+        with ImageImpulseRunner(modelfile) as runner:
+            try:
+                model_info = runner.init()
+                print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
+                labels = model_info['model_parameters']['labels']
+                if len(args)>= 2:
+                    videoCaptureDeviceId = int(args[1])
+                else:
+                    port_ids = get_webcams()
+                    if len(port_ids) == 0:
+                        raise Exception('Cannot find any webcams')
+                    if len(args)<= 1 and len(port_ids)> 1:
+                        raise Exception("Multiple cameras found. Add the camera port ID as a second argument to use to this script")
+                    videoCaptureDeviceId = int(port_ids[0])
+     
+                camera = cv2.VideoCapture(videoCaptureDeviceId)
+                ret = camera.read()[0]
+                if ret:
+                    backendName = camera.getBackendName()
+                    w = camera.get(3)
+                    h = camera.get(4)
+                    print("Camera %s (%s x %s) in port %s selected." %(backendName,h,w, videoCaptureDeviceId))
+                    camera.release()
+                else:
+                    raise Exception("Couldn't initialize selected camera.")
+     
+                next_frame = 0 # limit to ~10 fps here
+     
+                for res, img in runner.classifier(videoCaptureDeviceId):
+#                    if GPIO.input(IR_out) == False:
+                    if (next_frame > now()):
+                        time.sleep((next_frame - now()) / 1000)
+     
+                    # print('classification runner response', res)
+                    Recycling = "Recycling"
+                    Compost = "Compost"
+                    Metal = "Metal"
+                    acceptable_threshold = 0.8
+                    if "classification" in res["result"].keys():
+                        print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
+                        for label in labels:
+                           score = res['result']['classification'][label]
+                           if score > acceptable_threshold and label == Recycling:
+                                again = 0
+                                return recycle
+                           elif score > acceptable_threshold and label == Metal:
+                                again = 0
+                                return metal
+                           elif score > acceptable_threshold and label == Compost:
+                                again = 0
+                                return compost
+                           print('%s: %.2f\t' % (label, score), end='')
+                        print('', flush=True)
+     
+                    elif "bounding_boxes" in res["result"].keys():
+                        print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
+                        for bb in res["result"]["bounding_boxes"]:
+                            print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
+                            img = cv2.rectangle(img, (bb['x'], bb['y']), (bb['x'] + bb['width'], bb['y'] + bb['height']), (255, 0, 0), 1)
+     
+                    if (show_camera):
+                        cv2.imshow('edgeimpulse', cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+                        if cv2.waitKey(1) == ord('q'):
+                            break
+     
+                    next_frame = now() + 100
+            finally:
+                if (runner):
+                    runner.stop()
+ 
+def main(argv):
 
-            camera = cv2.VideoCapture(videoCaptureDeviceId)
-            ret = camera.read()[0]
-            if ret:
-                backendName = camera.getBackendName()
-                w = camera.get(3)
-                h = camera.get(4)
-                print("Camera %s (%s x %s) in port %s selected." %(backendName,h,w, videoCaptureDeviceId))
-                camera.release()
-            else:
-                raise Exception("Couldn't initialize selected camera.")
+    #while True: 
+        #p.ChangeDutyCycle(other)
+        again = 1
+        identify = imageProcessing(again, argv)
+        print("\nIdentify:", identify)
+        #rotate(home, identify)
+        #time.sleep(5)
+        #p.ChangeDutyCycle(home)
 
-            next_frame = 0 # limit to ~10 fps here
-
-            for res, img in runner.classifier(videoCaptureDeviceId):
-               if GPIO.input(IR_out) == False:
-                if (next_frame > now()):
-                    time.sleep((next_frame - now()) / 1000)
-
-                # print('classification runner response', res)
-                Recycling = "Recycling"
-                Compost = "Compost"
-                Metal = "Metal"
-                acceptable_threshold = 0.8
-                if "classification" in res["result"].keys():
-                    print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
-                    for label in labels:
-                       score = res['result']['classification'][label]
-                       if score > acceptable_threshold and label == Recycling:
-                          rotate(home, recycle)
-                          time.sleep(3)
-                          rotate(recycle, home)
-                          p.stop()
-                      # elif score > acceptable_threshold and label == Metal:
-                       #   rotate(recycle, metal)
-                       #elif score > acceptable_threshold and label == Compost:
-                        #  rotate(metal, compost)
-                       print('%s: %.2f\t' % (label, score), end='')
-                    print('', flush=True)
-
-                elif "bounding_boxes" in res["result"].keys():
-                    print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
-                    for bb in res["result"]["bounding_boxes"]:
-                        print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
-                        img = cv2.rectangle(img, (bb['x'], bb['y']), (bb['x'] + bb['width'], bb['y'] + bb['height']), (255, 0, 0), 1)
-
-                if (show_camera):
-                    cv2.imshow('edgeimpulse', cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
-                    if cv2.waitKey(1) == ord('q'):
-                        break
-
-                next_frame = now() + 100
-        finally:
-            if (runner):
-                runner.stop()
+ 
 
 if __name__ == "__main__":
    main(sys.argv[1:])
--- examples/image/motor_test.py
+++ examples/image/motor_test.py
@@ -10,33 +10,19 @@ GPIO.setup(servoPIN, GPIO.OUT)
 recycle = 4.5  
 metal = 6.5
 compost = 8.3
-other = 10   
+other = 10
+home = 4
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 18 for PWM with 50Hz
-p.start(2.5) # Initialization
+
 
 def rotate(previous, target):
     if previous < target:
         for i in np.arange (previous, target, 0.1):
             p.ChangeDutyCycle(i)
-            time.sleep(0.05)
+            time.sleep(0.04)
     else:
         for i in np.arange (previous, target, -0.1):
             p.ChangeDutyCycle(i)
-            time.sleep(0.05)
+            time.sleep(0.04)
 
-try:
-    rotate(other, recycle)
-    time.sleep(2)
-    rotate(recycle, metal)
-    time.sleep(2)
-    rotate(metal, compost)
-    time.sleep(2)
-    rotate(compost, other)
-    
-except KeyboardInterrupt:
-  p.stop()
-  #GPIO.cleanup()
-  
-finally:
-    GPIO.cleanup()

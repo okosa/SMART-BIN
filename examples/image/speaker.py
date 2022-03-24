d0bfdc476c1a5bfcb4051c1e0c9679088cd35b1c
okosa <okosadumebi@gmail.com>
2022-03-24 15:38:50 -0600

    speaker code

--- examples/image/speaker.py
+++ examples/image/speaker.py
@@ -1,3 +1,19 @@
-from espeak import espeak
+from num2words import num2words
+from subprocess import call
 
-espeak.synth("Hello Instructables!")
+def audio(text):
+
+    cmd_beg= 'espeak '
+    cmd_end= ' | aplay /home/pi/Desktop/Text.wav  2>/dev/null' # To play back the stored .wav file and to dump the std errors to /dev/null
+    cmd_out= '--stdout > /home/pi/Desktop/Text.wav ' # To store the voice file
+
+    #Replacing ' ' with '_' to identify words in the text entered
+    text = text.replace(' ', '_')
+
+    #Calls the Espeak TTS Engine to read aloud a Text
+    call([cmd_beg+cmd_out+text+cmd_end], shell=True)
+    
+    
+    
+
+audio("Nathan")
